c1fa350450cb8c0d611bbfa02c50fc8212995885
emraanarshad <emraanarshad710@gmail.com>
2026-05-13 16:05:17 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
-// Version 1.2
 self.addEventListener('install', (e) => {
   console.log('Service Worker Installed');
 });
-
 self.addEventListener('fetch', (e) => {
-  // Is se app offline bhi chal sakti hai
+  // Is ko khali chor dein taake functions block na hon
 });

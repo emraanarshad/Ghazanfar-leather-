52d314a71688b83659b63f7b0f0e86911bc5425e
emraanarshad <emraanarshad710@gmail.com>
2026-05-16 11:54:52 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
-self.addEventListener('fetch', (event) => {
-  // Is se site sahi chalti rahegi aur app install ho jayegi
+const CACHE_NAME = 'mizan-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
